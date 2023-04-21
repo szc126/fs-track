38decc50930033a2b137b300e73732fcdfcbfd99
szc126 <29127915+szc126@users.noreply.github.com>
2023-04-20 21:13:00 -0700

    shorter names

--- do.sh
+++ do.sh
@@ -21,38 +21,38 @@ all_caught_up=""
 
 for file in "${user_id}/page-"*".htm"
 do
-	#for fstion_render_url in $(grep -E "[^\"]+renderer[^\"]+" "${file}" --only-matching)
-	grep -P "renderer" "${file}" --after-context=3 --group-separator=$'\035' | while read -r -d $'\035' fstion_datax
+	#for fsn_render_url in $(grep -E "[^\"]+renderer[^\"]+" "${file}" --only-matching)
+	grep -P "renderer" "${file}" --after-context=3 --group-separator=$'\035' | while read -r -d $'\035' fsn_datax
 	do
-		fstion_render_url=$(echo "${fstion_datax}" | grep -E "[^\"]+renderer[^\"]+" --only-matching)
-		fstion_slug=$(echo "${fstion_datax}" | grep -P "[^/]+(?=#comments)" --only-matching)
-		fstion_modified=$(echo "${fstion_datax}" | grep -P "(?<=Last edited: <b>)([^<>]+)" --only-matching | sed -E "s/([0-9])(st|nd|rd|th)|,/\1/g")
-		fstion_modified_n=$(date --date="${fstion_modified}" "+%F")
-		fstion_modified_epoch=$(date --date="${fstion_modified}" "+%s")
+		fsn_render_url=$(echo "${fsn_datax}" | grep -E "[^\"]+renderer[^\"]+" --only-matching)
+		fsn_slug=$(echo "${fsn_datax}" | grep -P "[^/]+(?=#comments)" --only-matching)
+		fsn_modified=$(echo "${fsn_datax}" | grep -P "(?<=Last edited: <b>)([^<>]+)" --only-matching | sed -E "s/([0-9])(st|nd|rd|th)|,/\1/g")
+		fsn_modified_n=$(date --date="${fsn_modified}" "+%F")
+		fsn_modified_epoch=$(date --date="${fsn_modified}" "+%s")
 
-		fstion_id=$(echo "${fstion_render_url}" | grep -P "(?<=id=)[0-9]+" --only-matching)
-		fstion_v=$(echo "${fstion_render_url}" | grep -P "(?<=v=)[0-9a-z]+" --only-matching) # what is this anyway?
-		fstion_folder="${user_id}/${fstion_id} ${fstion_slug}"
-		fstion_out_file="${fstion_folder}/${fstion_modified_n}.json"
+		fsn_id=$(echo "${fsn_render_url}" | grep -P "(?<=id=)[0-9]+" --only-matching)
+		fsn_v=$(echo "${fsn_render_url}" | grep -P "(?<=v=)[0-9a-z]+" --only-matching) # what is this anyway?
+		fsn_folder="${user_id}/${fsn_id} ${fsn_slug}"
+		fsn_out_file="${fsn_folder}/${fsn_modified_n}.json"
 
 		# the fontstruction was renamed.
 		# find the existing folder
-		fstion_folder_existing=$(find -wholename "./${user_id}/${fstion_id} *" -type d)
-		if [ "./${fstion_folder}" != "${fstion_folder_existing}" ] && [ "${fstion_folder_existing}" != "" ]
+		fsn_folder_existing=$(find -wholename "./${user_id}/${fsn_id} *" -type d)
+		if [ "./${fsn_folder}" != "${fsn_folder_existing}" ] && [ "${fsn_folder_existing}" != "" ]
 		then
-			mv "${fstion_folder_existing}" "${fstion_folder}" --verbose --no-clobber
+			mv "${fsn_folder_existing}" "${fsn_folder}" --verbose --no-clobber
 		fi
 
 		case "${verb}" in
 			"init")
-				mkdir "${fstion_folder}"
-				wget "https://fontstruct.com/api/1/fontstructions/${fstion_id}?fast=1&v=${fstion_v}&_cacheable_fragment=1" --output-document="${fstion_out_file}"
+				mkdir "${fsn_folder}"
+				wget "https://fontstruct.com/api/1/fontstructions/${fsn_id}?fast=1&v=${fsn_v}&_cacheable_fragment=1" --output-document="${fsn_out_file}"
 				;;
 			"update")
-				if [ "${fstion_modified_epoch}" -ge "${last_dl_date_epoch}" ]
+				if [ "${fsn_modified_epoch}" -ge "${last_dl_date_epoch}" ]
 				then
-					mkdir --parents "${fstion_folder}"
-					wget "https://fontstruct.com/api/1/fontstructions/${fstion_id}?fast=1&v=${fstion_v}&_cacheable_fragment=1" --output-document="${fstion_out_file}"
+					mkdir --parents "${fsn_folder}"
+					wget "https://fontstruct.com/api/1/fontstructions/${fsn_id}?fast=1&v=${fsn_v}&_cacheable_fragment=1" --output-document="${fsn_out_file}"
 				else
 					all_caught_up="true"
 					break
@@ -65,11 +65,11 @@ do
 			break
 		fi
 
-		#fstion_slug=$(jq ".slug" "${fstion_out_file}" -r)
-		fstion_modified_seconds_n=$(jq ".modified" "${fstion_out_file}" -r | sed "s/://g")
-		fstion_out_file_new="${fstion_folder}/${fstion_modified_seconds_n}.json"
+		#fsn_slug=$(jq ".slug" "${fsn_out_file}" -r)
+		fsn_modified_seconds_n=$(jq ".modified" "${fsn_out_file}" -r | sed "s/://g")
+		fsn_out_file_new="${fsn_folder}/${fsn_modified_seconds_n}.json"
 
-		mv "${fstion_out_file}" "${fstion_out_file_new}" --verbose --no-clobber
+		mv "${fsn_out_file}" "${fsn_out_file_new}" --verbose --no-clobber
 	done
 done
 
